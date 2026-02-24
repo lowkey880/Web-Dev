d5493399c5de45f00c2f78599156a58e8928e4da
Kanshin Arseniy <arseniykanshin@gmail.com>
2026-02-25 00:21:42 +0500

    добавил новую папку

---
+++ lab4/task2/online-store/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css'],
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+  shareToWhatsApp() {
+    const text = `Check out this product: ${this.product.link}`;
+    const url = `https://wa.me/?text=${encodeURIComponent(text)}`;
+    window.open(url, '_blank');
+  }
+
+  shareToTelegram() {
+    const url = `https://t.me/share/url?url=${encodeURIComponent(
+      this.product.link
+    )}&text=${encodeURIComponent(this.product.name)}`;
+    window.open(url, '_blank');
+  }
+}
